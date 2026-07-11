40a75a5a9db48b3d831248f3544d918f8fcf4718
madhuri_mc <madhuriforwork@gmail.com>
2026-07-11 21:20:00 +0530

    astro sitemap and meta updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://frontendguide-dev.vercel.app/', // your real live URL
+  integrations: [sitemap()],
+});
